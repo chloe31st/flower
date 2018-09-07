59d79bb91a361c5bb25f61d51e062d1059e960a4
陳　思予 <chensiyu@ownernoMacBook-Pro.local>
2018-09-07 17:54:28 +0900

    client html修正

---
+++ set/index.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+
+$('form#uploadForm').submit(function() {
+
+  search('https://script.google.com/macros/s/AKfycbzB6lBa6MyqKkr-j3NFQLEhs-X5mJ-KSqzKU5MJQbWXnvG-G_g5/exec', $('div#imi'), $('div#imi'));
+
+  return false;
+  return false;
+
+
+});
+
+
+
+function search(resource, jqsucc, jqerr) {
+
+  $.ajax({
+    type:     "POST",
+    url:      resource,
+    dataType: "file",
+    success:  function(data, textStatus, jqXHR) {
+                jqsucc.text(data+"okayです");
+              },
+    error:    function(jqXHR, textStatus, errorThrown) {
+                jqerr.text(jqXHR+"no okayです");
+              }
+    });
+
+}
+
+
+
+
+});
+
+//算数
+// $('form#aa').submit(function() {
+//
+//   var bb =$('form#aa [name=bb]').val();
+//   var cc =$('form#aa [name=cc]').val();
+//   var ans =parseInt(bb)+parseInt(cc);
+//   var str =""+ans;
+//
+//   //search('https://script.google.com/macros/s/AKfycbzB6lBa6MyqKkr-j3NFQLEhs-X5mJ-KSqzKU5MJQbWXnvG-G_g5/exec', $('div#imi'), $('div#imi'));
+//   $('div#imi').text(ans);
+//   return false;
+//   return false;
+//
+// });
+// });
