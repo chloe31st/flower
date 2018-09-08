69e9c5b70a881a8efb0e186748ebb7257a83c9a2
Quei-An Chen <kwea123@gmail.com>
2018-09-08 17:01:36 +0800

    修正

--- set/index.js
+++ set/index.js
@@ -1,28 +1,25 @@
 $(document).ready(function() {
 
 $('form#uploadForm').submit(function() {
-
   search('https://script.google.com/macros/s/AKfycbzB6lBa6MyqKkr-j3NFQLEhs-X5mJ-KSqzKU5MJQbWXnvG-G_g5/exec', $('div#imi'), $('div#imi'));
-
   return false;
   return false;
-
-
 });
 
 
+});
 
 function search(resource, jqsucc, jqerr) {
 
   $.ajax({
     type:     "POST",
     url:      resource,
-    dataType: "file",
+    dataType: "text",
     success:  function(data, textStatus, jqXHR) {
-                jqsucc.text(data+"okayです");
+                jqsucc.text(data+"okay");
               },
     error:    function(jqXHR, textStatus, errorThrown) {
-                jqerr.text(jqXHR+"no okayです");
+                jqerr.text(jqXHR+"no okay");
               }
     });
 
@@ -30,8 +27,6 @@ function search(resource, jqsucc, jqerr) {
 
 
 
-
-});
 
 //算数
 // $('form#aa').submit(function() {
